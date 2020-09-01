911d04d544302f53ea32ed003c258e3097eb45ad
Vishal M <vishalmaurya958@gmail.com>
2020-09-01 07:30:41 -0400

    some minor changes

--- Recognize Faces Base/base.py
+++ Recognize Faces Base/base.py
@@ -4,16 +4,16 @@ import face_recognition
 
 
 
-imgElon = face_recognition.load_image_file('./ImageBasic/elon1.jpg')
-imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+imgKnown = face_recognition.load_image_file('./ImageBasic/elon1.jpg')
+imgKnown = cv2.cvtColor(imgKnown,cv2.COLOR_BGR2RGB)
 
 
 imgTest = face_recognition.load_image_file('./ImageBasic/elon2.jpg')
 imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
-faceloc = face_recognition.face_locations(imgElon)[0]
-encodeElon = face_recognition.face_encodings(imgElon)[0]
-cv2.rectangle(imgElon,(faceloc[3],faceloc[0]),(faceloc[1],faceloc[2]),(255,0,0),2)
+faceloc = face_recognition.face_locations(imgKnown)[0]
+encodeElon = face_recognition.face_encodings(imgKnown)[0]
+cv2.rectangle(imgKnown,(faceloc[3],faceloc[0]),(faceloc[1],faceloc[2]),(255,0,0),2)
 
 
 facelocTest = face_recognition.face_locations(imgTest)[0]
@@ -25,7 +25,7 @@ facedist = face_recognition.face_distance([encodeElon],encodeTest)
 print(results,facedist)
 
 cv2.putText(imgTest,f'{results}{round(facedist[0],2)}', (50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
-cv2.imshow('Elon',imgElon)
+cv2.imshow('Elon',imgKnown)
 cv2.imshow('Elon Test',imgTest)
 cv2.waitKey(0)
 
